6a46b688e8e3cabd5a24bf02cc088c94dc3a7ccb
Anis Emad <anisemad123@gmail.com>
2023-03-17 16:44:27 +0000

    added a file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * 
+  * Return: 0 if executed sucessfully, 1 if not
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
