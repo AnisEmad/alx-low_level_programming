26361aaf1700971739a64d4ca3b21d0b382e5d17
Anis Emad <anisemad123@gmail.com>
2023-03-17 17:02:04 +0000

    added a file

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: 0 if executed successfully, 1 if not
+  */
+int main(void)
+{
+	char ch = 'a';
+	int n = '1';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+		n++;
+	}
+	for (i = 0; i < 6; i++)
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
