ca776e79b7d2593369d6630c00a52db5955cf3b1
Anis Emad <anisemad123@gmail.com>
2023-03-17 16:54:33 +0000

    added a file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: 0 if executed sucessfully, 1 if not
+  */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
