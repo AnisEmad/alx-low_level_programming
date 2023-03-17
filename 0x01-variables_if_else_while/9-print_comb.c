a54a885aa930c16f041c74cf25417b94e5858d93
Anis Emad <anisemad123@gmail.com>
2023-03-17 17:07:17 +0000

    added a file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  *
+  * Return: 0 if executed sucessfully, 1 if not
+  */
+int main(void)
+{
+	int n = '0';
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
