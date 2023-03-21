30b8e88f8b8b5c828ebd1d26f388ca605d2241c9
Anis Emad <anisemad123@gmail.com>
2023-03-21 13:43:42 +0000

    fix

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
   * _islower - checks if the character is lower case or not
-  * &c - the character in asci code
+  * @c: the character in asci code
   * Return: 1 if it's lower.
   * 0 if it's not.
   */
