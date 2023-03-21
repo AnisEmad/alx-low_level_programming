ff98e7de5bf96a3dfe8532768de64fc474b7e000
Anis Emad <anisemad123@gmail.com>
2023-03-21 14:51:14 +0000

    added two files

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+  * jack_bauer - prints every minute of the day
+  *
+  * Return: void
+  */
+void jack_bauer(void)
+{
+	int min1 = 0;
+	int min2 = 0;
+	int h1 = 0;
+	int h2 = 0;
+	
+	while (h1 != 2 && h2 != 3 && min1 != 5 && min2 != 9)
+	{
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(min1 + '0');
+		_putchar(min2 + '0');
+		_putchar('\n');
+		min2++;
+		if (min2 == 10)
+		{
+			min2 = 0;
+			min1++;
+		}
+		if (min1 == 6)
+		{
+			min1 = 0;
+			h2++;
+		}
+		if (h2 == 10)
+		{
+			h2 = 0;
+			h1++;
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,52 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
-/**
-  * _putchar - prints the character given
-  * @c : the character printed
-  *
-  * Return: ON sucess 1.
-  */
+
 int _putchar(char c);
-/**
-  * print_alphabet - prints the the alphabet followed by newline
-  *
-  * Return: void
-  */
 void print_alphabet(void);
-/**
-  * print_alphabet_x10 - prints the alphabet characters 10 times in 10 lines followed by newline
-  *
-  * Return: void
-  */
 void print_alphabet_x10(void);
-/**
-  * _islower - checks if the character is lowercase or not
-  * @c : the character checked 
-  *
-  * Return: 1 if it's lowercase
-  * 0 if it's not.
-  */
 int _islower(int c);
-/**
-  * _isalpha - checks if the character is alphabet character or not
-  * @c: the character being checked
-  * Return: 1 if it's alphabet character.
-  * 0 if it is not.
-  */
 int _isalpha(int c);
-/**
-  * prin_sign - print the sign of the number
-  * @n: the number being checked
-  * Return: 1 if it is greater than zero.
-  * 0 if it is equal to zero.
-  * -1 if it is less than zero.
-  */
 int print_sign(int n);
-/**
-  * _abs - computes the absolute value of an integer
-  * @n: the number used 
-  * Return: the absolute value
-  */
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
+
 #endif
