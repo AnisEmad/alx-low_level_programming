fcbb9227ffa227097928937bd6611139e79a7058
Anis Emad <anisemad123@gmail.com>
2023-03-28 17:46:33 +0000

    added a file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _strlen: compute the length of the string
+  * @s: Pointer to the string ( array of char)
+  * Return: length of the string in int
+  */
+
+int _strlen(char *s)
+{
+	return (sizeof(s)/8);
+}
