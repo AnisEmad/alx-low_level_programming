aa64b4f490d00fa4b455e96a87e52693d3b87296
Anis Emad <anisemad123@gmail.com>
2023-03-28 18:08:03 +0000

    added a file

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+  * print_rev - prints a string in reverse order 
+  * @s: pointer to string 
+  * Return: void
+  */
+
+void print_rev(char *s)
+{
+	int cnt = 0;
+	
+	while (*s != '\0')
+	{
+		cnt++;
+		s++;
+	}
+	while (cnt > 0)
+	{
+		s--;
+		_putchar(*s);
+		cnt--;
+	}
+	_putchar('\n');
+}
