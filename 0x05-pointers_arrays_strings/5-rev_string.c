d522f6256a76b568b2ba02882638e4c84b416d7b
Anis Emad <anisemad123@gmail.com>
2023-03-28 18:48:49 +0000

    added a file

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  * rev_string - reverse a string
+  * @s: pointer to string
+  * Return: void
+  */
+
+void rev_string(char *s)
+{
+	int cnt = 0;
+	char *str;
+
+	while (*s != '\0')
+	{
+		cnt++;
+		s++;
+	}
+	while (cnt > 0)
+	{
+		s--;
+		*str = s*;
+		cnt--;
+	}
+	s = str;
+}
