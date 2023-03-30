c31aa23b788fcf4949319f250aa1ad37dea9a319
Anis Emad <anisemad123@gmail.com>
2023-03-30 13:59:56 +0000

    fix

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -14,14 +14,14 @@ int _strcmp(char *s1, char *s2)
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] > s2[i])
-			return (1);
+			return (s1[i] - s2[i]);
 		else if (s1[i] < s2[i])
-			return (-1);
+			return (s2[i] - s1[i]);
 	}
 	if (s1[i] == '\0' && s2[i] == '\0')
 		return (0);
 	else if (s1[i] == '\0')
-		return (-1);
+		return (s2[i]);
 	else
-		return (1);
+		return (s1[i]);
 }
