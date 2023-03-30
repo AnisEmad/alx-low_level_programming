3b9bea298e7d7e2cbc4c2ffd2d2593759b67bc0b
Anis Emad <anisemad123@gmail.com>
2023-03-30 14:58:45 +0000

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -14,8 +14,12 @@ char *rot13(char *str)
 	int j;
 
 	for (i = 0; i < str[i] != '\0'; i++)
+	{
 		for (j = 0; j < 52; j++)
+		{
 			if (str[i] == input[j])
 				str[i] = output[j];
+		}
+	}
 	return (str);
 }
