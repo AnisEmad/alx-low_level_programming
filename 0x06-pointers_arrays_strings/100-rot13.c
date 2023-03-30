278d79e4fab1e88fa9677cc51317907892c46cdd
Anis Emad <anisemad123@gmail.com>
2023-03-30 14:55:48 +0000

    added a file

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * rot13 - encodes a string using rot13
+  * @str: string used
+  * Return: pointer to string
+  */
+
+char *rot13(char *str)
+{
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+	int i;
+	int j;
+
+	for (i = 0; i < str[i] != '\0'; i++)
+		for (j = 0; j < 52; j++)
+			if (str[i] == input[j])
+				str[i] = output[j];
+	return (str);
+}
