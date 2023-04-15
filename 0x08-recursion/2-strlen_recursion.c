59bc68be69a6ebdad4f6257000fde8bd4ef63558
Anis Emad <anisemad123@gmail.com>
2023-04-15 13:44:03 +0000

    add

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+  * _strlen_recursion - compute teh length of a string
+  * @s : pointer to a string
+  * Return: int elngth of a string 
+  */
+int _strlen_recursion(char *)
+{
+	if (*s == '\0')
+		return 0;
+	return 1 + _strlen_recursion(++s);
+}
