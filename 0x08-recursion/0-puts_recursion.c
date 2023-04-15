427667f86e30b627f96c244950c29bcd957cf03b
Anis Emad <anisemad123@gmail.com>
2023-04-15 13:02:59 +0000

    added two files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+  * _puts_recursion - prints a string follwed by a new line
+  * @s : pointer to the string
+  * Return: void
+  */
+void _puts_recursion(char *s)
+{
+	if (s == '\0')
+		return;
+	_putchar(*s);
+	s--;
+	_puts_recursion(s);
+}
