31abf9c6548d92e2a549327fabbe696317fab2e6
Anis Emad <anisemad123@gmail.com>
2023-06-07 18:33:52 +0000

    added a file

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+  * prime_help - helper function
+  * @n: number checked
+  * @m: number used to help
+  * Return: 1 if it's prime, otherwise 0
+  */
+
+int prime_help(int n, int m)
+{
+	if (m == 1 && n != 1)
+		return (1);
+	if (n % m == 0 || m == 1)
+		return (0);
+	return (prime_help(n, m - 1));
+}
+/**
+  * is_prime_number - checks if the number is prime or not
+  * @n: the number used
+  * Return: 1 if it's prime, otherwise 0
+  */
+
+int is_prime_number(int n)
+{
+	return (prime_help(n, n - 1));
+}
