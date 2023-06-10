19f4a1c36aed80a51cfa253e89b02b4ffc3261b5
Anis Emad <anisemad123@gmail.com>
2023-06-10 02:19:19 +0000

    add

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * main - entry point
+  * @argc: number of args
+  * @argv: array of strings
+  * Return: always 0
+  */
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	if (argc - 1 < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+
+	printf("%d\n", n1 * n2);
+	return (0);
+}
