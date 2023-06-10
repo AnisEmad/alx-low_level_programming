60e1922fa973bbd8a7a853edffea1e6b4a2770c0
Anis Emad <anisemad123@gmail.com>
2023-06-10 01:58:07 +0000

    added file

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * @argc: the number of arguments
+  * @argv: an array of strings
+  * Return: always 0
+  */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
