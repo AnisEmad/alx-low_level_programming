3912f401e94fddeb286037330b56d5008ca156fc
Anis Emad <anisemad123@gmail.com>
2023-06-13 07:29:05 +0000

    fix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,11 +17,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	arr = (char *) malloc(size * sizeof(char));
 
+	if (arr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
 	}
-	if (arr == NULL)
-		return (NULL);
 	return (arr);
 }
