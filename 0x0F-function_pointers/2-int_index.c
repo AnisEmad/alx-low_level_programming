bbc32890284f92c40b2ee5879294f1146cb3e1c7
Anis Emad <anisemad123@gmail.com>
2023-06-21 15:03:35 +0000

    fix

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -9,12 +9,12 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
+	int i;
+
 	if (array && size && cmp)
 	{
 		if (size <= 0)
 			return (-1);
-		int i;
-
 		for (i = 0; i < size; i++)
 		{
 			if (cmp(array[i]) != 0)
