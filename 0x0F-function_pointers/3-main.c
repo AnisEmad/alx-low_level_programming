815e4891688de500eb96099a316b6e84bf097fbe
Anis Emad <anisemad123@gmail.com>
2023-06-21 16:14:30 +0000

    add 4 files

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+  * main - entry point
+  * @argc: argument count
+  * @argv: argument array
+  * Return: always zero
+  */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2;
+	int (*p)(int, int);
+
+	if (argc != 4 && argv[2] + 1 == '\0')
+	{
+		printf("Error = argc\n");
+		exit(98);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	p = get_op_func(argv[2]);
+	if (p == NULL)
+	{
+		printf("Error = null\n");
+		exit(99);
+	}
+	if (n2 == 0 && (p == op_div || p == op_mod))
+	{
+		printf("Error == div\n");
+		exit(100);
+	}
+	printf("%d\n", p(n1, n2));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+  * op_add - add two numbers
+  * @a: 1st number
+  * @b: 2nd number
+  * Return: the sum of them
+  */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * op_sub - subtracte two numbers
+  * @a: 1st number
+  * @b: 2nd number
+  * Return: the subtraction of them
+  */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * op_mul - multiply two numbers
+  * @a: 1st number
+  * @b: 2nd number
+  * Return: the mutiplcation of them
+  */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * op_div - divide two numbers
+  * @a: 1st number
+  * @b: 2nd number
+  * Return: the divison of them
+  */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/** 
+  * op_mod - compute the modulo 
+  * @a: 1st number
+  * @b: 2nd number
+  * Return: modulo 
+  */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
