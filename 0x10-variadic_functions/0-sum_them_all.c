4a8800effdfd5ad6e5766731696e5116885513e1
Anis Emad <anisemad123@gmail.com>
2023-06-22 14:47:59 +0000

    add

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include "variadic_functions.h"
+/**
+  * sum_them_all - sum all of its parameters
+  *
+  * @n: the number of parameters
+  * Return: the sum of the parameters
+  */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ap;
+	int sum;
+	unsigned int i;
+
+	va_start(ap, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+		sum += va_arg(ap, int);
+
+	va_end(ap);
+	return (sum);
+}
+
--- README.md
+++
@@ -1,1 +0,0 @@
-This is all I learned in c programming language from ALX
