16a58b9dcf61bdb8b8063c139f67a81c91c40057
Anis Emad <anisemad123@gmail.com>
2023-07-08 07:27:13 +0000

    binary representation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+  * print_binary - prints the binary representation of anumber
+  * @n: number used
+  * Return: void
+  */
+void print_binary(unsigned long int n)
+{
+	unsigned long int i;
+	int flag = 0;
+	
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	for (i = 1 << 31; i > 0; i = i / 2)
+	{
+		if ((n & i) && !flag)
+		{
+			flag = 1;
+			_putchar('1');
+		}else if (flag)
+		{
+			(n & i) ? _putchar('1') : _putchar('0');
+		}
+	}
+}
