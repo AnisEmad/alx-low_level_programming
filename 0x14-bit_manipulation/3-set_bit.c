c2872a1b98f730621e8cbd21eea7c1785c56ea7f
Anis Emad <anisemad123@gmail.com>
2023-07-08 08:26:39 +0000

    fix betty documentaion

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -3,12 +3,12 @@
   * set_bit - set the value of a bit to 1
   * @n: the number used
   * @index: the index of the specfic bit
-  * Return 1 on success -1 on failer
+  * Return: 1 on success -1 on failer
   */
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int mask = 1UL << index;
-	
+
 	if (index > 63)
 		return (-1);
 	*n |= mask;
