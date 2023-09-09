3918e42dcc233c051d4fb8f22d066f37a68e5b33
Anis Emad <anisemad123@gmail.com>
2023-09-09 15:17:55 +0300

    fixed betty

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -3,15 +3,15 @@
  * key_index - it should hash the key into an index
  * @key: this is the key used in the function
  * @size: the size of the array
- * 
+ *
  * Return: the index produced
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    unsigned long int index;
+	unsigned long int index;
 
-    index = hash_djb2(key);
+	index = hash_djb2(key);
 
-    return (index % size);
+	return (index % size);
 }
