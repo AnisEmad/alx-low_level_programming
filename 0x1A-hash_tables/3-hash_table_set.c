2d44106a6de3746886e77a59a86487d22dca4fe4
Anis Emad <anisemad123@gmail.com>
2023-09-10 15:47:50 +0300

    fix

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -36,8 +36,19 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new;
 	else
 	{
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{
+			if (strcmp(ht->array[index]->value, value) == 0)
+				return (1);
+			else
+			{
+				strcpy(ht->array[index]->value, value);
+				return (1);
+			}
+		}
 		new->next = ht->array[index];
 		ht->array[index] = new;
 	}
+
 	return (1);
 }
